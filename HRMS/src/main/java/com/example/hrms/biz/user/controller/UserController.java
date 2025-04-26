3644bf95a5c9c13b62dd1a5e7ffe42fe458591a4
HolyLightEnvoy <holy.light.envoy@gmail.com>
2025-04-26 12:31:10 +0700

    office

---
+++ HRMS/src/main/java/com/example/hrms/biz/user/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.example.hrms.biz.user.controller;
+
+import com.example.hrms.biz.booking.model.Booking;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class UserController {
+
+    @RequestMapping("")
+    public String openUserView(Model model) {
+        model.addAttribute("user", new Booking());
+        return "user";
+    }
+    @GetMapping("/")
+    public String showLoginPage() {
+        return "login";  // Return the name of your login view
+    }
+    @RequestMapping("/users/login")
+    public String loginPage() {
+        return "login"; // Trả về trang login.html
+    }
+    @RequestMapping("/users/home")
+    public String homePage(Model model) {
+        return "index";
+    }
+    @RequestMapping("/users/profile")
+    public String profilePage() {
+        return "profile";
+    }
+    @RequestMapping("/users/change-password")
+    public String change() {
+        return "change-password";
+    }
+    
+    @RequestMapping("/user/office")
+    public String office() {
+        return "office";
+    }
+}
